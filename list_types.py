3b8e57ad0d4f2fd321ff84b2f8e1dd48ae323c78
Brycio <brycelangridge@gmail.com>
2025-04-12 11:14:37 +0200

    Added 3 files, list_types is my code

---
+++ list_types.py
@@ -0,0 +1,13 @@
+# Create a list of 3 friends
+friends_names = ['Nick', 'Leah', 'Sims']
+
+# Print names of friends and length
+print(friends_names[0])
+print(friends_names[2])
+print(len(friends_names))
+
+# Age of friends
+friends_ages = [28, 23, 29]
+print(f"{friends_names[0]} is {friends_ages[0]} years old.")
+print(f"{friends_names[1]} is {friends_ages[1]} years old.")
+print(f"{friends_names[2]} is {friends_ages[2]} years old.")
